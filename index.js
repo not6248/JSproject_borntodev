abd4218d49550092a36db3ece4d44c7578ea8c1b
not6248 <not-6248@hotmail.com>
2023-04-01 13:32:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 
 
 function WantTo_wakeUp(h,m,c){
-    let hours = parseInt(h);
-    let minutes = parseInt(m);
     let Cycles = c*90;
     const timeInMinutes = parseInt(h) * 60 + parseInt(m);
     const CyclesCalculat = timeInMinutes - (Cycles);  //6 Cycles 9 * 60
+    return CyclesCalculat;
     // const Cycles5 = timeInMinutes - (Cycles);  //5 Cycles 7H 30M (7.5 * 60)
     // const Cycles4 = timeInMinutes - (Cycles);  //4 Cycles 6 * 60
     // const Cycles3 = timeInMinutes - (Cycles);  //3 Cycles 4H 30M (4.5 * 60)
-    return CyclesCalculat;
 }
 
 
@@ -24,11 +22,16 @@ function minuteToTime(minutes){
       return hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0');
 }
 
+function Display(h,m){
+    console.log(minuteToTime(WantTo_wakeUp(h,m,6)));
+    console.log(minuteToTime(WantTo_wakeUp(h,m,5)));
+    console.log(minuteToTime(WantTo_wakeUp(h,m,4)));
+    console.log(minuteToTime(WantTo_wakeUp(h,m,3)));
 
-console.log(minuteToTime(WantTo_wakeUp(6,30,6)));
-console.log(minuteToTime(WantTo_wakeUp(6,30,5)));
-console.log(minuteToTime(WantTo_wakeUp(6,30,4)));
-console.log(minuteToTime(WantTo_wakeUp(6,30,3)));
+}
+
+Display(6,30);
+
 // console.log(minuteToTime(Cycles5));
 // console.log(minuteToTime(Cycles4));
 // console.log(minuteToTime(Cycles3));
